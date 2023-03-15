21d9164a69eed7e42b5d9a18c3c5ce98aa0cbff7
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2023-03-14 23:33:52 -0300

    commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from '../constants'
+
+export function getRandomPrompt(propmt){
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
+  const randomPrompt = surpriseMePrompts[randomIndex]
+
+  if (randomPrompt === prompt) return getRandomPrompt(propmt)
+
+  return randomPrompt
+} 
