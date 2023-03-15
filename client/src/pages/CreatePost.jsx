900a8184524053abe8c5de956045ddbc6481255c
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2023-03-15 19:54:23 -0300

    Generate Image

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,12 +30,17 @@ const CreatePost = () => {
         
         const data = await response.json();
 
-        setForm({ ...form, photo: 'data:image/jpeg;base64, ${data.photo})
+        setForm({ ...form, photo: `data:image/jpeg;base64, ${data.photo}`})
       } catch (error){
-
+        alert(error);
+        } finally {
+          setGeneratingImg(false);
+        }
+      } else {
+        alert('Please enter a prompt')
       }
     }
-  }
+  
 
   const handleSubmit = () => {
 
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -10,7 +10,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-const openai = new OpenAIApi(Configuration);
+const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
   res.send('Hello from DALL-E!')
@@ -30,9 +30,10 @@ router.route('/').post(async (req, res) => {
     const image = aiResponse.data.data[0].b64_json;
 
     res.status(200).json({ photo: image })
+
   } catch (error){
     console.log(error);
-    res.status(500).send(error?.response.data.error.message);
+    res.status(500).send(error?.response.data.error.message, 'aca');
 
   }
 })
